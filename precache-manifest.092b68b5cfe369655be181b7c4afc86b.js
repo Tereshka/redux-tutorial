875cf446791fb5146c91bba707a6372d402809ed
Tereshka <tereshka07@gmail.com>
2020-04-17 10:45:03 +0900

    Updates

---
+++ precache-manifest.092b68b5cfe369655be181b7c4afc86b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919e76447895c2249f72592def8f5d24",
+    "url": "/redux-tutorial/index.html"
+  },
+  {
+    "revision": "6708f5f4ffcbadd37b51",
+    "url": "/redux-tutorial/static/css/main.bc6507c5.chunk.css"
+  },
+  {
+    "revision": "ee42f8444ade4967c8d7",
+    "url": "/redux-tutorial/static/js/2.93e8af71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-tutorial/static/js/2.93e8af71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6708f5f4ffcbadd37b51",
+    "url": "/redux-tutorial/static/js/main.f6290e73.chunk.js"
+  },
+  {
+    "revision": "37ef833f9cbd54c04126",
+    "url": "/redux-tutorial/static/js/runtime-main.419aa553.js"
+  }
+]);
